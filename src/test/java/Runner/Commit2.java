a2ebf0f50f3115b6b8c31308fc1af1128fdaa5d6
leyder12 <sarmientoleyder.12@gmail.com>
2024-06-14 13:28:53 -0500

    commit 2 para el reto

---
+++ src/test/java/Runner/Commit2.java
@@ -0,0 +1,8 @@
+package Runner;
+
+public class Commit2 {
+    public static void main(String[] args) {
+
+        System.out.println("Commit 2");
+    }
+}
--- src/test/java/Runner/Reto.java
+++ src/test/java/Runner/Reto.java
@@ -1,5 +1,37 @@
 package Runner;
 
 public class Reto {
-    
+    public static void main(String[] args) {
+
+        CuentaBancaria cuenta = new CuentaBancaria("Leyder", 100); //Objeto con nueva cuenta, saldo inicial $100
+        cuenta.depositar(50); //Deposito $50
+        cuenta.retirar(30); //Retirar 30
+        System.out.println("Saldo actual de la cuenta: $" + cuenta.getSaldo());
+    }
 }
+class CuentaBancaria {
+    private String titular;
+    private double saldo;
+    public CuentaBancaria(String t, double s) {
+        titular = t;
+        saldo = s;
+    }
+    public void depositar(double cantidad) {
+        saldo += cantidad;
+    }
+    public void retirar(double cantidad) {
+        if (cantidad <= saldo) {
+            saldo -= cantidad;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
+
+
+
+
